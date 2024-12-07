5b0af4b0f1226748cbe428472707a6c6f3c1e69d
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-07 14:58:16 +0100

    TailwindCSS Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: '#1e3a8a',
+                secondary: '#9333ea',
+            },
         },
     },
     plugins: [],
