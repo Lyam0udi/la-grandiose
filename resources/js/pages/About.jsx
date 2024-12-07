28ca22ae18f4d2d9f102a39c51a775c69b99d241
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-07 16:37:06 +0100

    Initialization of About content

--- resources/js/pages/About.jsx
+++ resources/js/pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+// resources/js/pages/About.jsx
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+    const { t } = useTranslation();
 
-export default About
+    return (
+        <div className="about py-16 bg-lightBackground">
+            <div className="max-w-7xl mx-auto text-center">
+                <h2 className="text-3xl font-bold">{t('about_us')}</h2>
+                <p className="mt-4 text-xl">{t('about_description')}</p>
+                <div className="mt-8">
+                    <p className="text-gray-700">{t('about_details')}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
