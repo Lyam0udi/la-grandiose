322d3e42a8cd287681790d46b5b44446094bc2c4
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-19 16:00:35 +0100

    Initial Authentification

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin0000'), // Replace with a strong password
+            //'role_id' => 1, // Assuming 1 is the admin role ID
+        ]);
+    }
+}
