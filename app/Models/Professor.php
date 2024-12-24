204f0142a9e5917da37256a9250cbe3dd106e1d9
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-24 10:06:16 +0100

    Fixe the Professor model

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    //
+    protected $fillable = ['photo'];
+
+    public function translations()
+    {
+        return $this->hasMany(ProfessorTranslation::class);
+    }
 }
