08bb5f6afbc19c3642ab2226c1e23108293a73bf
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-24 10:08:34 +0100

    Fixe the Professor_translation model

--- app/Models/ProfessorTranslation.php
+++ app/Models/ProfessorTranslation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProfessorTranslation extends Model
 {
-    //
+    protected $fillable = ['locale', 'name', 'study_material', 'description'];
+
+    public function professor()
+    {
+        return $this->belongsTo(Professor::class);
+    }
 }
+
