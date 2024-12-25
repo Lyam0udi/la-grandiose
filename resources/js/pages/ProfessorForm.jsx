012ed436222245c123b58c1b31d9961d29e61566
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-25 14:37:42 +0100

    Fixing bug import i18n on professor page

--- resources/js/pages/ProfessorForm.jsx
+++ resources/js/pages/ProfessorForm.jsx
@@ -3,6 +3,8 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n';
+
 
 export default function ProfessorForm({ professor = null, locales }) {
     const { data, setData, post, put, processing, errors } = useForm({
